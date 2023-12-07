0d560c3a9ca537f3a56321a054ea2dc0e67ea6d3
Shane Alex Pereira <73254673+shane-pereira@users.noreply.github.com>
2023-12-07 14:16:13 +0530

    Update 12-rock-paper-scissors.js
    Uncommented gtm code

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -1,4 +1,4 @@
-// runGTM();
+runGTM();
 
 let score = JSON.parse(localStorage.getItem('score')) || {
   wins: 0,
@@ -142,13 +142,13 @@ function resetMoveDisplay() {
   document.querySelector('.js-result').innerHTML = '';
 }
 
-// GTM function
-// function runGTM() {
-//     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-//   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-//   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-//   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-//   })(window,document,'script','dataLayer','GTM-K5DLHF6V');
-// }
-  // <!-- Google Tag Manager - Header-->
-  // <!-- End Google Tag Manager Header Code-->
+GTM function
+function runGTM() {
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-K5DLHF6V');
+}
+  <!-- Google Tag Manager - Header-->
+  <!-- End Google Tag Manager Header Code-->
