a8aa42bdc1e4e6c26ae2845defc1e6ce59232157
Shane Alex Pereira <73254673+shane-pereira@users.noreply.github.com>
2023-12-07 13:42:04 +0530

    Update 12-rock-paper-scissors.js
    Put GTM code inside runGTM function
    Executed runGTM function inside autoPlay and playGame functions

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -1,11 +1,4 @@
-  // Implementing GTM on page Load
-  // <!-- Google Tag Manager - Header-->
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-K5DLHF6V');
-  // <!-- End Google Tag Manager Header Code-->
+runGTM();
 
 let score = JSON.parse(localStorage.getItem('score')) || {
   wins: 0,
@@ -51,6 +44,7 @@ function autoPlay() {
     intervalID = setInterval( () => {
       const playerMove = pickComputerMove();
       playGame(playerMove);
+      runGTM();
     }, 1000);
     isAutoPlaying = true;
     document.querySelector('.js-auto-play-score-button').innerHTML = 'Stop Playing'
@@ -112,6 +106,8 @@ function playGame(playerMove) {
   document.querySelector('.js-result').innerHTML = result;
 
   displayCurrentMove(playerMove,computerMove);
+
+  runGTM();
 }
 
 function pickComputerMove() {
@@ -145,3 +141,14 @@ function displayCurrentMove (playerMove,computerMove) {
 function resetMoveDisplay() {
   document.querySelector('.js-result').innerHTML = '';
 }
+
+// GTM function
+function runGTM() {
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-K5DLHF6V');
+}
+  // <!-- Google Tag Manager - Header-->
+  // <!-- End Google Tag Manager Header Code-->
