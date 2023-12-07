3a51233543da045a4b89c4237e797e6f9523af84
Shane Alex Pereira <73254673+shane-pereira@users.noreply.github.com>
2023-12-07 14:17:51 +0530

    Update 12-rock-paper-scissors.js

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -142,7 +142,7 @@ function resetMoveDisplay() {
   document.querySelector('.js-result').innerHTML = '';
 }
 
-GTM function
+// GTM function
 function runGTM() {
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
