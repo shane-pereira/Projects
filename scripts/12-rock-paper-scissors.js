1676e7b99cbb0f206e808a6972f219924d8e2ff7
Shane Alex Pereira <73254673+shane-pereira@users.noreply.github.com>
2023-12-07 12:27:04 +0530

    Update 12-rock-paper-scissors.js

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -1,18 +1,11 @@
   // Implementing GTM on page Load
-  <!-- Google Tag Manager - Header-->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  // <!-- Google Tag Manager - Header-->
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-K5DLHF6V');</script>
-  <!-- End Google Tag Manager -->
-
-  <!-- Google Tag Manager (noscript) - Body -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K5DLHF6V"
-  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  <!-- End Google Tag Manager (noscript) -->
-
-    // End of GTM Code
+  })(window,document,'script','dataLayer','GTM-K5DLHF6V');
+  // <!-- End Google Tag Manager Header Code-->
 
 let score = JSON.parse(localStorage.getItem('score')) || {
   wins: 0,
